f4caf33599072d1e544c4c6c9f5f267d3e1cdf66
Jérémy Jourdan <github@kema.dev>
2025-07-27 19:07:01 +0200

    up

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -6,17 +6,34 @@ package log
 import (
 	"log/slog"
 	"os"
+
+	"github.com/kemadev/go-framework/pkg/config"
+	semconv "go.opentelemetry.io/otel/semconv/v1.34.0"
 )
 
 // CreateFallbackLogger returns a fallback logger that is used when the OpenTelemetry logger is not available.
 // It uses the slog package to create a JSON logger that writes to stdout.
-func CreateFallbackLogger() *slog.Logger {
-	return slog.New(
-		slog.NewJSONHandler(
+func CreateFallbackLogger(conf config.Runtime) *slog.Logger {
+	var handler slog.Handler
+	if conf.IsLocalEnvironment() {
+		handler = slog.NewTextHandler(os.Stdout,
+			&slog.HandlerOptions{
+				Level:     slog.LevelDebug,
+				AddSource: true,
+			},
+		)
+	} else {
+		handler = slog.NewJSONHandler(
 			os.Stdout,
 			&slog.HandlerOptions{
-				Level: slog.LevelDebug,
+				Level:     slog.LevelInfo,
+				AddSource: true,
 			},
-		),
+		)
+	}
+	return slog.New(handler).With(
+		slog.String(string(semconv.ServiceNameKey), conf.AppName),
+		slog.String(string(semconv.ServiceNamespaceKey), conf.AppNamespace),
+		slog.String(string(semconv.ServiceVersionKey), conf.AppVersion),
 	)
 }
