c42b8fee14eba9e509a660d86dcd3d141c3c7f7f
Jérémy Jourdan <github@kema.dev>
2025-07-28 20:15:37 +0200

    up

---
+++ pkg/otel/doc.go
@@ -0,0 +1,3 @@
+// Package otel intends to provide helper functions to setup the OpenTelemetry SDK, in a conventionnal
+// maner and tailoring the configuration to the application's own configuration.
+package otel
