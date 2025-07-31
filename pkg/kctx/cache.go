187d1e75d5ff9515c23df36fdbb443b891712174
Jérémy Jourdan <github@kema.dev>
2025-07-31 17:09:47 +0200

    up

---
+++ pkg/kctx/cache.go
@@ -0,0 +1,117 @@
+package kctx
+
+import (
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/kemadev/go-framework/pkg/header"
+)
+
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Cache-Control#cache_directives
+type CacheHeader struct {
+	MaxAge               time.Duration
+	MaxStale             time.Duration
+	MinFresh             time.Duration
+	SMaxAge              time.Duration
+	NoCache              bool
+	NoStore              bool
+	NoTransform          bool
+	OnlyIfCached         bool
+	MustRevalidate       bool
+	ProxyRevalidate      bool
+	MustUnderstand       bool
+	Private              bool
+	Public               bool
+	Immutable            bool
+	StaleWhileRevalidate bool
+	StaleIfError         bool
+}
+
+// CacheDecision represents a decision on how to handle caching
+type CacheDecision int
+
+const (
+	// Allow caching
+	CacheAllow CacheDecision = iota
+	// Revalidate cache
+	CacheRevalidate
+	// Refresh cache entry
+	CacheRefresh
+	// Skip cache
+	CacheBypass
+)
+
+// Build constructs the Cache-Control header value for given CacheHeader
+func (head *CacheHeader) Build() string {
+	if head == nil {
+		return ""
+	}
+
+	reasonableHeadValueSize := 32
+
+	var b strings.Builder
+	b.Grow(reasonableHeadValueSize)
+
+	head.addDuration(&b, "max-age", head.MaxAge)
+	head.addDuration(&b, "max-stale", head.MaxStale)
+	head.addDuration(&b, "min-fresh", head.MinFresh)
+	head.addDuration(&b, "s-maxage", head.SMaxAge)
+
+	head.addDirective(&b, "no-cache", head.NoCache)
+	head.addDirective(&b, "no-store", head.NoStore)
+	head.addDirective(&b, "no-transform", head.NoTransform)
+	head.addDirective(&b, "only-if-cached", head.OnlyIfCached)
+	head.addDirective(&b, "must-revalidate", head.MustRevalidate)
+	head.addDirective(&b, "proxy-revalidate", head.ProxyRevalidate)
+	head.addDirective(&b, "must-understand", head.MustUnderstand)
+	head.addDirective(&b, "private", head.Private)
+	head.addDirective(&b, "public", head.Public)
+	head.addDirective(&b, "immutable", head.Immutable)
+	head.addDirective(&b, "stale-while-revalidate", head.StaleWhileRevalidate)
+	head.addDirective(&b, "stale-if-error", head.StaleIfError)
+
+	return b.String()
+}
+
+func (head *CacheHeader) addDirective(b *strings.Builder, directive string, enabled bool) {
+	if !enabled {
+		return
+	}
+
+	if b.Len() > 0 {
+		b.WriteString(", ")
+	}
+
+	b.WriteString(directive)
+}
+
+func (head *CacheHeader) addDuration(b *strings.Builder, directive string, duration time.Duration) {
+	if duration <= 0 {
+		return
+	}
+
+	head.addDirective(b, directive, true)
+	b.WriteByte('=')
+	b.WriteString(strconv.Itoa(int(duration.Seconds())))
+}
+
+// CachePolicySet sets cache control header for given cace directive
+func (c *Kctx) CachePolicySet(head CacheHeader) {
+	c.w.Header().Set(header.CacheControl, head.Build())
+}
+
+func (c *Kctx) CacheDecision(r *http.Response) CacheDecision {
+	return CacheAllow
+}
+
+// Age
+// Etag
+// Last-Modified
+// Vary
+// If-Match
+// If-Modified-Since
+// If-None-Match
+// If-Range
+// If-Unmodified-Since
--- pkg/kctx/request.go
+++ pkg/kctx/request.go
@@ -1,21 +1,9 @@
 package kctx
 
 import (
-	"strconv"
-	"strings"
 	"time"
 
 	"github.com/kemadev/go-framework/pkg/header"
-)
-
-// CacheDirective represents cache handling instructions
-type CacheDirective int
-
-const (
-	CacheAllow CacheDirective = iota
-	CacheRevalidate
-	CacheBypass
-	CacheForceRefresh
 )
 
 // Date returns the Date of the request, based on "Date" header, as a [time.Date]. If an error occurs,
@@ -26,55 +14,4 @@ func (c *Kctx) Date() time.Time {
 		return time.Time{}
 	}
 	return date
-}
-
-// CachePolicy determines how to handle caching based on request headers
-func (c *Kctx) CachePolicy() CacheDirective {
-	cacheControl := c.r.Header.Get(header.CacheControl)
-
-	if cacheControl != "" {
-		switch {
-		case strings.Contains(cacheControl, "no-cache"):
-			return CacheForceRefresh
-		case strings.Contains(cacheControl, "no-store"):
-			return CacheBypass
-		case strings.Contains(cacheControl, "max-age=0"):
-			return CacheRevalidate
-		case strings.Contains(cacheControl, "must-revalidate"):
-			return CacheRevalidate
-		}
-	}
-
-	return CacheAllow
-}
-
-// IsStale checks if cached content should be considered stale
-func (c *Kctx) IsStale(cacheTime time.Time) bool {
-	return time.Since(cacheTime) > c.getMaxAge()
-}
-
-func (c *Kctx) getMaxAge() time.Duration {
-	cacheControl := c.r.Header.Get(header.CacheControl)
-	if cacheControl == "" {
-		return 0
-	}
-
-	maxAgePrefix := "max-age="
-	if idx := strings.Index(cacheControl, maxAgePrefix); idx != -1 {
-		start := idx + len(maxAgePrefix)
-		end := start
-		for end < len(cacheControl) && cacheControl[end] >= '0' && cacheControl[end] <= '9' {
-			end++
-		}
-		if end > start {
-			seconds, err := strconv.Atoi(cacheControl[start:end])
-			if err != nil {
-				return 0
-			}
-
-			return time.Duration(seconds) * time.Second
-		}
-	}
-
-	return 0
 }
