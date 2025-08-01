6dc13b09452974eb64fff76354d955bd0a5de27a
Jérémy Jourdan <github@kema.dev>
2025-08-01 15:22:31 +0200

    up

--- pkg/router/doc.go
+++ pkg/router/doc.go
@@ -1,4 +1,9 @@
 // Copyright 2025 kemadev
 // SPDX-License-Identifier: MPL-2.0
 
+// Package router implements a simple HTTP router with middleware capabilities, using 0 dependency.
+// It mostly is a shameless copy of [this nice article] by [Alex Edwards]
+//
+// [this nice article]: https://www.alexedwards.net/blog/organize-your-go-middleware-without-dependencies
+// [Alex Edwards]: https://www.alexedwards.net/
 package router
